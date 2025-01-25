fix speed calculation

utils/video.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 def process_frame(capture, scale=None):
     ret, frame = capture.read()
     if ret:
@@ -22,9 +23,45 @@ def map_to_homography(position, h_matrix):
     return p_world[0], p_world[1]
 
 
+def map_from_homography(position, h_matrix):
+    """
+     We use the inverted matrix to map homographic point to real world points
+    :param position: the tutor line position
+    :param h_matrix: the homography matrix
+    :return:
+    """
+    inv_h_matrix = np.linalg.inv(h_matrix)
+    p_array = [position[0], position[1], 1]
 
-def print_car_speed(frame, speed, rect_coords):
-    (x, y, w, h) = rect_coords
+    p_world = np.dot(inv_h_matrix, p_array)
+    p_world /= p_world[2]
+    return int(p_world[0]), int(p_world[1])
+
+
+def draw_optical_flow(frame, flow):
+    h, w = flow.shape[:2]
+    for y in range(0, h, 16):
+        for x in range(0, w, 16):
+            fx, fy = flow[y, x]
+            magnitude = np.sqrt(fx**2 + fy**2)
+
+            # Only draw if magnitude exceeds threshold
+            if magnitude > 2:
+                x_end = int(x + fx)
+                y_end = int(y + fy)
+                cv2.arrowedLine(
+                    frame,
+                    (x, y),
+                    (x_end, y_end),
+                    (0,255, 0),
+                    thickness=1,
+                    line_type=cv2.LINE_AA,
+                    tipLength=0.3
+                )
+
+
+def print_car_speed(frame, speed, rect_coords, max_allowed_speed):
+    (x, y, x1, y1) = rect_coords
 
     # Set text properties
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -32,16 +69,13 @@ def print_car_speed(frame, speed, rect_coords):
     font_thickness = 2
     text_color = (0, 0, 255)  # Red color in BGR
 
-    speed_text = f'{speed} km/h'
-    (text_width, text_height), baseline = cv2.getTextSize(speed_text, font, font_scale, font_thickness)
-    text_x = x + (w - text_width) // 2  # Center horizontally
-    text_y = y + text_height // 2  # Center vertically (adjust for baseline)
+    speed_text = f'{speed:.2f} km/h'
+
+    rect_color = (0, 255, 0) if speed <= max_allowed_speed else (0, 0, 255)
 
     # Drawing box rectangle
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    # Drawing speed text
-    cv2.rectangle(frame, (x, y - 10), (x + w, y + 10), (0, 255, 0), -1)
-    cv2.putText(frame, speed_text, (text_x, text_y - 5), font, font_scale, text_color, font_thickness)
+    cv2.rectangle(frame, (x, y), (x1, y1), rect_color, 2)
+    cv2.putText(frame, speed_text, (x, y - 5), font, font_scale, text_color, font_thickness)
 
 
 def is_using_gpu():